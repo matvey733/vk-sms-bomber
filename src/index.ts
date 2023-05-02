d94e4acfa0def1edb00b76a5d48a025791c09301
Matvey <morozovmatvej75@gmail.com>
2023-05-02 16:40:30 +0300

    send a response on root url

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,7 @@ import { bomb } from "./bomb";
 const app = express();
 const PORT = process.env.PORT ?? 3001;
 
+app.get("/", (req, res) => res.send("root"));
 app.get("/bomb", bomb);
+
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
