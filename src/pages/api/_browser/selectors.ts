c46747f5e9973975e38837fb7cf8f118cd95693c
Matvey <morozovmatvej75@gmail.com>
2023-05-12 19:57:03 +0300

    remove unused selectors

--- src/pages/api/_browser/selectors.ts
+++ src/pages/api/_browser/selectors.ts
@@ -1,13 +1,7 @@
 const selectors = {
   phoneNumberInput: "#reset > div > section > div > form > div.vkuiFormItem.vkuiInternalFormItem.vkuiFormItem--sizeY-none.vkuiInternalFormItem--sizeY-none > span > input",
   firstConfirmBtn: "#root > div > div > div.vkuiSplitLayout__inner.vkuiSplitLayout__inner--header.App__layout > div > div > div > div > div > div.vkuiSplitLayout__inner > section > div > div > div > div > div > section > div.vkuiDiv.CUAPhoneIntro__footer-common--skocjj > button > span.vkuiButton__in > span",
-  secondConfirmBtn: "#root > div > div > div.vkuiSplitLayout__inner.vkuiSplitLayout__inner--header.App__layout > div > div > div > div > div > div.vkuiSplitLayout__inner > section > div > div > div > div > div > div.CUACentered-common--skocjj > section > div > form > div.vkuiFormItem.vkuiInternalFormItem.vkuiFormItem--sizeY-none.vkuiInternalFormItem--sizeY-none.PhoneVerifyIntroButton__desktop-common--skocjj > button > span.vkuiButton__in > span",
-  afterCalling: {
-    sendSmsBtn: "#root > div > div > div.vkuiSplitLayout__inner.vkuiSplitLayout__inner--header.App__layout > div > div > div > div > div > div.vkuiSplitLayout__inner > section > div > div > div > div > div > section > div > form > div.vkuiFormItem.vkuiInternalFormItem.vkuiFormItem--sizeY-none.vkuiInternalFormItem--sizeY-none.CUAProcessContainer__actions-common--skocjj.CUAProcessContainer__actions--desktop-common--skocjj > button"
-  },
-  afterSentTextMsg: {
-    sendSmsBtn: "#root > div > div > div.vkuiSplitLayout__inner.vkuiSplitLayout__inner--header.App__layout > div > div > div > div > div > div.vkuiSplitLayout__inner > section > div > div > div > div > div > section > div > form > div.vkuiFormItem.vkuiInternalFormItem.vkuiFormItem--sizeY-none.vkuiInternalFormItem--sizeY-none.CUAProcessContainer__actions-common--skocjj.CUAProcessContainer__actions--desktop-common--skocjj > button.vkuiButton.vkuiButton--size-m.vkuiButton--mode-tertiary.vkuiButton--appearance-accent.vkuiButton--align-center.vkuiButton--sizeY-none.vkuiButton--android.vkuiTappable.vkuiInternalTappable.vkuiTappable--sizeX-none.vkuiTappable--hasHover.vkuiTappable--hasActive"
-  }
+  secondConfirmBtn: "#root > div > div > div.vkuiSplitLayout__inner.vkuiSplitLayout__inner--header.App__layout > div > div > div > div > div > div.vkuiSplitLayout__inner > section > div > div > div > div > div > div.CUACentered-common--skocjj > section > div > form > div.vkuiFormItem.vkuiInternalFormItem.vkuiFormItem--sizeY-none.vkuiInternalFormItem--sizeY-none.PhoneVerifyIntroButton__desktop-common--skocjj > button > span.vkuiButton__in > span"
 }
 
 export default selectors;
