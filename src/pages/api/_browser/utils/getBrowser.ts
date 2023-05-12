c626b011528854625c659132b8142f93cbbd26fb
Matvey <morozovmatvej75@gmail.com>
2023-05-12 19:17:24 +0300

    throw if a required env var is undefined

--- src/pages/api/_browser/utils/getBrowser.ts
+++ src/pages/api/_browser/utils/getBrowser.ts
@@ -4,13 +4,16 @@ export default async function getBrowser(options: { headless: boolean }): Promis
   const NODE_ENV = process.env.NODE_ENV;
   const BROWSER_EXEC_PATH = process.env.BROWSER_EXEC_PATH;
   const BROWSER_CONNECT_URL = process.env.BROWSER_CONNECT_URL;
+  console.log(NODE_ENV);
 
   if (NODE_ENV === "development") {
+    if (!BROWSER_EXEC_PATH) throw new Error("no BROWSER_EXEC_PATH provided");
     return puppeteer.launch({
       executablePath: BROWSER_EXEC_PATH,
       headless: options.headless
     });
   } else if (NODE_ENV === "production") {
+    if (!BROWSER_CONNECT_URL) throw new Error("no BROWSER_CONNECT_URL provided");
     return puppeteer.connect({ browserURL: BROWSER_CONNECT_URL });
   } else {
     throw new Error("unknown environment. Possible environments are: 'development', 'production'");
